c963b2704cd09e69a4459ec12d1e87f4d4a4b9f1
jrpatton1985 <patton.jimmy85@gmail.com>
2017-07-05 17:24:27 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
-exports.client_id = '1ffe2f1b28614847bfb8e7df41890b7a';
-exports.client_secret = 'd72c8250f1c34a21b87c6c5a83024b1a';
+// Register your own app with Spotify API to obtain client id and client secret keys
+exports.client_id = 'CLIENT_ID';
+exports.client_secret = 'CLIENT_SECRET';
 exports.redirect_uri = 'http://localhost:4200';
